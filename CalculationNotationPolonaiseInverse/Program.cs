8072bf05fd198a252dad96407d35d3b2d02e5bf8
Enzo Duquesne <enzo.duquesne@etud.u-picardie.fr>
2024-01-18 15:21:38 +0100

    ça marche ou ff

--- CalculationNotationPolonaiseInverse/Program.cs
+++ CalculationNotationPolonaiseInverse/Program.cs
@@ -44,6 +44,11 @@ class RPNCalculator
 
     static double CalculateRPN(string input)
     {
+        if (input == null)
+        {
+            throw new ArgumentNullException(nameof(input));
+        }
+
         string[] tokens = input.Split(' ');
         Stack<double> stack = new Stack<double>();
 
